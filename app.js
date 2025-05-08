38166c20ad193d724180233d9e0dcf422dd0e3bb
Logan Akins <lakins2@gmail.com>
2025-05-08 13:08:34 -0400

    JS Intro exercise Complete

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
 
 // Exercise 2
+const firstName = "Logan"; 
+lastName = "A";
+age = 35;
 
 // Exercise 3
+let language = "Javescript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//exercise 4
+let price = 19.99
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+//used console.log in browser console to check work, complete.
